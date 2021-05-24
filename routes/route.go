f595c6fab532de2cb2e6c03eef425c5c98be7181
duytq <duybkit13@gmail.com>
2021-05-24 21:37:30 +0700

    update go routes

--- routes/route.go
+++ routes/route.go
@@ -1,11 +1,24 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
-func SetupRoutes(route *gin.Engine) string {
-	route.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+func SetupRoutes(router *gin.Engine) {
+	api := router.Group("/api")
+	{
+
+		v1 := api.Group("/v1")
+		{
+			admin := v1.Group("/admin")
+			{
+				admin.GET("/ping", func(c *gin.Context) {
+					c.JSON(200, gin.H{
+						"message": "pong",
+					})
+				})
+			}
+
+		}
+	}
 }
